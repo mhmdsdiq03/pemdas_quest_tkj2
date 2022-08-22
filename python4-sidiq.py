749e8c877c6561918d90f35d23a7c44d5d9f74bc
mhmdsdiq03 <111480212+mhmdsdiq03@users.noreply.github.com>
2022-08-21 22:12:44 -0700

    Add files via upload

---
+++ python4-sidiq.py
@@ -0,0 +1,17 @@
+print("hallo semuanya")
+nama_saya_adalah=input("\nnama saya adalah")
+print("\nHari ini kita sekolah")
+print("kita akan belajar mengkonveksi celcius ke fahrenheit")
+print("kita mulai bahas rumusnya dulu ya \nrumus celcius")
+
+a= int(input("masukan nilai A: "))
+b= float(input("masukan nilai B: "))
+c= a + b
+
+print("A =", a)
+print("B =", b)
+print(a,"+",b,"=", c)
+
+x = int(input("masukan suhu celcius"))
+y = (9/5 * x) + 32
+print(x, "celcius = ", y, "fahrenheit")
